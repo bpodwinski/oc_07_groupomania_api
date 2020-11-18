1268c708a659bfc4d654a8391f5999a49c844108
Benoit Podwinski <me@benoitpodwinski.com>
2020-11-18 11:20:03 +0100

    change cors origin

--- middlewares/cors.ts
+++ middlewares/cors.ts
@@ -2,7 +2,8 @@ import * as cors from "cors";
 
 export default class Cors {
   public cors = cors({
-    origin: "*",
+    origin: "http://crios:8080",
+    credentials: true,
     methods: "GET,PUT,POST,DELETE",
     allowedHeaders:
       "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
